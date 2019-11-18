cade7088d9d587e9d878ffb5e6a0aaaf760ce3da
Alexander-DD <38096839+Alexander-DD@users.noreply.github.com>
2019-11-18 20:59:37 +0300

    t

---
+++ ETFTestTask/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ETFTestTask
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.connectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// treeView1
+			// 
+			this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.treeView1.Location = new System.Drawing.Point(0, 24);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(524, 497);
+			this.treeView1.TabIndex = 0;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectionToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(524, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// connectionToolStripMenuItem
+			// 
+			this.connectionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToolStripMenuItem});
+			this.connectionToolStripMenuItem.Name = "connectionToolStripMenuItem";
+			this.connectionToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+			this.connectionToolStripMenuItem.Text = "Connection";
+			// 
+			// connectToolStripMenuItem
+			// 
+			this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+			this.connectToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.connectToolStripMenuItem.Text = "Connect";
+			this.connectToolStripMenuItem.Click += new System.EventHandler(this.connectToolStripMenuItem_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(524, 521);
+			this.Controls.Add(this.treeView1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "ETFTestTaskDolgikh";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem connectionToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+	}
+}
+
---
+++ ETFTestTask/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ETFTestTask
+{
+	public partial class Form1 : Form
+	{
+		private string connectionString = "Server= localhost\\sqlexpress; Database= ETFTestTaskBase; Integrated Security=True;";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void connectToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+
+					String sql = "SELECT id, parent_id, name FROM catalog_level;";
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								int id = reader.GetSqlInt32(0).Value;
+								System.Data.SqlTypes.SqlInt32 parent_id = reader.GetSqlInt32(1);
+
+								string name = reader.GetString(2);
+
+								treeView1.BeginUpdate();
+
+								if (parent_id.IsNull)
+								{
+									treeView1.Nodes.Add(id.ToString(), name);
+								}
+								else
+								{
+									TreeNode[] treeNodes = treeView1.Nodes.Find(parent_id.Value.ToString(), true);
+									for (int i = 0; i < treeNodes.Length; i++)
+									{
+										treeNodes[i].Nodes.Add(id.ToString(), name);
+									}
+								}
+								treeView1.EndUpdate();
+							}
+						}
+					}
+				}
+			}
+			catch (SqlException exc)
+			{
+				Console.WriteLine(exc.ToString());
+			}
+		}
+	}
+}
